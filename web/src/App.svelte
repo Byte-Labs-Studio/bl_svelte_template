0db2563017af81ab42716779aa18f10e3d56b116
Xirvin <xander228irvin@gmail.com>
2024-01-25 08:44:46 +1100

    ref(Debug Component): Now dynamically imports Debug Component only if in browser

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
     import { CONFIG, IS_BROWSER } from './stores/stores';
-    import AlwaysListened, { InitialiseListen } from '@utils/listeners';
+    import { InitialiseListen } from '@utils/listeners';
     import Visibility from '@providers/Visibility.svelte';
-    import Debug from '@providers/Debug.svelte';
     import ImageHolder from '@components/ImageHolder.svelte';
 
     CONFIG.set({
@@ -19,5 +17,7 @@
 </Visibility>
 
 {#if $IS_BROWSER}
-    <Debug />
+    {#await import('./providers/Debug.svelte') then { default: Debug }}
+        <Debug />
+    {/await}
 {/if}
