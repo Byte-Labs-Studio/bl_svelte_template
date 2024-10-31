c7c7a8b5f07c439ffd12d1933770e47439b2e930
Frowmza <66181451+Frowmza@users.noreply.github.com>
2024-10-31 16:28:04 +0100

    fix: component class to function (v5 migration)

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,8 +1,6 @@
 import './app.css'
-import App from './App.svelte'
-
-const app = new App({
-  target: document.getElementById('app'),
-})
+import { mount } from 'svelte';
+import App from './App.svelte';
+const app = mount(App, { target: document.getElementById("app") as HTMLElement  });
 
 export default app
